9515f93d6c6616cfc041131d91dc8074d4c8bbe5
Mitchell Cash <mitchell.cash@gmail.com>
2015-07-07 20:51:58 +1000

    Create a simple bash script to update the node to the latest version

---
+++ UpdatePaycoinNode.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "### Changing to paycoind directory"
+cd ~
+echo "### Stopping Paycoin Server"
+./paycoind stop
+echo "### Changing to home directory"
+cd ~
+echo "### Downloading Paycoin Core 0.3.0.2"
+wget https://github.com/PaycoinFoundation/paycoin/releases/download/v0.3.0.2/linux64.zip
+echo "### Installing Paycoin Core 0.3.0.2"
+rm -f -r paycoind
+unzip linux64.zip
+rm -f -r linux64.zip
+rm -f -r paycoin-qt
+echo "### System will now reboot"
+reboot
