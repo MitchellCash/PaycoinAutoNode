8f41bf8ee43932e37741fe230e329175224db955
sk00t3r <johnsone@roadrunner.com>
2015-11-04 22:03:14 -0500

    Update DeployWebInterface.sh

--- DeployWebInterface.sh
+++ DeployWebInterface.sh
@@ -42,7 +42,7 @@ echo "### Installing the Web Interface"
 sudo python WebInterface.py
 echo "### Changing to home directory"
 cd ~
-echo "### Scheduling Cron Job to run WebInterface.py every 5 minutes"
+echo "### Scheduling Cron Job to run WebInterface.py every 1 minute"
 (crontab -l ; echo "*/1 * * * * sudo python ~/WebInterface/WebInterface.py")| crontab -
 echo "### System will now reboot"
 reboot
