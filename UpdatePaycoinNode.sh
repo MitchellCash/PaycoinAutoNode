9a4284348b92b52054f684a7508ae07e592e500f
sk00t3r <johnsone@roadrunner.com>
2015-11-14 15:40:00 -0500

    Update UpdatePaycoinNode.sh

--- UpdatePaycoinNode.sh
+++ UpdatePaycoinNode.sh
@@ -5,9 +5,9 @@ echo "### Stopping Paycoin Server"
 ./paycoind stop
 echo "### Changing to home directory"
 cd ~
-echo "### Downloading Paycoin Core 0.3.2.0"
-wget https://github.com/PaycoinFoundation/paycoin/releases/download/v0.3.2.0/linux64.zip
-echo "### Installing Paycoin Core 0.3.2.0"
+echo "### Downloading Paycoin Core 0.3.3.0"
+wget https://github.com/PaycoinFoundation/paycoin/releases/download/v0.3.3.0/linux64.zip
+echo "### Installing Paycoin Core 0.3.3.0"
 rm -f -r paycoind
 unzip linux64.zip
 rm -f -r linux64.zip
