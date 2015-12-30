ecf74d4db9a294c88475bef4cb9df9e8656ee64b
Mitchell Cash <mitchell.cash@gmail.com>
2015-12-31 09:14:25 +1000

    Fix pylint error ID C0326 in server_uptime.py and enable in Travis

--- server_uptime.py
+++ server_uptime.py
@@ -4,7 +4,7 @@ import os
 def server_uptime():
 
      try:
-         f = open( "/proc/uptime" )
+         f = open("/proc/uptime")
          contents = f.read().split()
          f.close()
      except:
@@ -13,21 +13,21 @@ def server_uptime():
      total_seconds = float(contents[0])
 
      # Helper vars:
-     MINUTE  = 60
-     HOUR    = MINUTE * 60
-     DAY     = HOUR * 24
+     MINUTE = 60
+     HOUR = MINUTE * 60
+     DAY = HOUR * 24
 
      # Get the days, hours, etc:
-     days    = int( total_seconds / DAY )
-     hours   = int( ( total_seconds % DAY ) / HOUR )
-     minutes = int( ( total_seconds % HOUR ) / MINUTE )
+     days = int(total_seconds / DAY)
+     hours = int((total_seconds % DAY) / HOUR)
+     minutes = int((total_seconds % HOUR) / MINUTE)
 
      # Build up the pretty string (like this: "N days, N hours, N minutes, N seconds")
      string = ""
      if days > 0:
-         string += str(days) + " " + (days == 1 and "day" or "days" ) + ", "
+         string += str(days) + " " + (days == 1 and "day" or "days") + ", "
      if len(string) > 0 or hours > 0:
-         string += str(hours) + " " + (hours == 1 and "hour" or "hours" ) + " and "
+         string += str(hours) + " " + (hours == 1 and "hour" or "hours") + " and "
      if len(string) > 0 or minutes > 0:
-         string += str(minutes) + " " + (minutes == 1 and "minute" or "minutes" )
+         string += str(minutes) + " " + (minutes == 1 and "minute" or "minutes")
      return string;
